edf819a30cdbf73e193e133b46a35b936a8765f1
Abdul4real <137358627+Abdul4real@users.noreply.github.com>
2023-09-13 19:27:40 -0400

    add classes and modules

---
+++ module/lib.js
@@ -0,0 +1,4 @@
+function halfOf(x){
+    return x/2;
+}
+export function multipl
---
+++ module/main.js
@@ -0,0 +1,3 @@
+import { halfOf,multipl } from "./lib.js";
+console.log("halfOf:" + halfOf(84));
+console.log("multiply:" + multipl(84));
